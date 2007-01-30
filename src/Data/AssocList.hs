ad2dce328f128536bb3357d8467baed7ac93cb14
uwe <uwe@fh-wedel.de>
2007-01-30 11:19:55 +0100

    documentation for Data.AssocList added
    darcs-hash:20070130101955-be05e-b6f7afa45bdc01ddc66ec0ea5070c75d09c1c75e.gz

--- src/Data/AssocList.hs
+++ src/Data/AssocList.hs
@@ -15,25 +15,34 @@ type AssocList k v = [(k, v)]
 
 -- lookup 	= lookup from Prelude
 
+-- | lookup with default value
+
 lookupDef	:: Eq k => v -> k -> AssocList k v -> v
 lookupDef d k	= fromMaybe d . lookup k
+
+-- | lookup with empty list (empty string) as default value
 
 lookup1		:: Eq k => k -> AssocList k [e] -> [e]
 lookup1 k	= fromMaybe [] . lookup k
 
+-- | test for existence of a key
+
 hasEntry	:: Eq k => k -> AssocList k v -> Bool
 hasEntry k	= isJust . lookup k
 
--- addEntry is strict
+-- | add an entry, remove an existing entry before adding the new one at the top of the list, addEntry is strict
+
 addEntry	:: Eq k => k -> v -> AssocList k v -> AssocList k v
 addEntry k v l	= ( (k,v) : ) $! delEntry k l
 
  -- let l' = delEntry k l in seq l' ((k, v) : l')
 
+-- | add a whole list of entries with 'addEntry'
+
 addEntries	:: Eq k => AssocList k v -> AssocList k v -> AssocList k v
 addEntries	= foldr (.) id . map (uncurry addEntry) . reverse
 
--- delEntry is strict
+-- | delete an entry, delEntry is strict
 delEntry	:: Eq k => k -> AssocList k v -> AssocList k v
 delEntry _ []	= []
 delEntry k (x@(k1,_) : rest)
@@ -41,6 +50,8 @@ delEntry k (x@(k1,_) : rest)
     | otherwise	= ( x : ) $! delEntry k rest
 
 -- delEntry k	= filter ((/= k) . fst)
+
+-- | delete a list of entries with 'delEntry'
 
 delEntries	:: Eq k => [k] -> AssocList k v -> AssocList k v
 delEntries	= foldl (.) id . map delEntry
