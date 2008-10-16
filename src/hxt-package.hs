e38a13e4189f3fcfd5fed482481273da18bd814e
uwe <uwe@fh-wedel.de>
2008-10-16 12:17:09 +0200

    option -O2 added to hxt.cabal
    darcs-hash:20081016101709-be05e-725114780685e218f3d79e14e0a77d01ad2e65ef.gz

--- src/hxt-package.hs
+++ src/hxt-package.hs
@@ -67,12 +67,12 @@ part4a dir
 part4b :: [String]
 part4b
     = [ " hs-source-dirs: src"
-      , " ghc-options: -Wall"
+      , " ghc-options: -Wall -O2"
       , " extensions: MultiParamTypeClasses DeriveDataTypeable FunctionalDependencies FlexibleInstances"
       , ""
       , " build-depends: base, haskell98 >= 1, parsec >= 2.1 && < 3, HUnit >= 1.2, network >= 2.1, tagsoup >= 0.6, curl >= 1.3"
       , " if flag(splitBase)"
-      , "   build-depends: base >= 3, containers >= 0.1, directory >= 1, filepath >= 1"
+      , "   build-depends: base >= 3 && < 4, containers >= 0.1, directory >= 1, filepath >= 1"
       , " else"
       , "   build-depends: base < 3"
       ]
