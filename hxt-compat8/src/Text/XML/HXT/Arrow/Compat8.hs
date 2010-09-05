a95642bdc81a3bd7c4e0b055d41d519f820876b9
Theo Lingen <theo@sichel.welt.all>
2010-09-05 17:13:18 +0200

    hxt-compat8 started

---
+++ hxt-compat8/src/Text/XML/HXT/Arrow/Compat8.hs
@@ -0,0 +1,227 @@
+-- ------------------------------------------------------------
+
+{- |
+   Module     : Text.XML.HXT.Arrow.Compat8
+   Copyright  : Copyright (C) 2010 Uwe Schmidt
+   License    : MIT
+
+   Maintainer : Uwe Schmidt (uwe@fh-wedel.de)
+   Stability  : experimental
+   Portability: portable
+
+   compatibility module for HXT-8.5
+
+-}
+
+-- ------------------------------------------------------------
+
+module Text.XML.HXT.Arrow.Compat8
+    ( module Text.XML.HXT.Arrow.Compat8
+    , module Text.XML.HXT.Arrow
+    )
+where
+
+import Text.XML.HXT.Arrow hiding ( readDocument
+                                 , readFromDocument
+                                 , readString
+                                 , readFromString
+                                 , xunpickleDocument
+                                 )
+
+import qualified Text.XML.HXT.Arrow as X
+
+-- ------------------------------------------------------------
+
+optionToSysConfig		:: (String, String) -> SysConfig
+optionToSysConfig (n, v)
+    | n == a_trace              	= withTrace      	    (toInt 0 v)
+    | n == a_issue_warnings		= withWarnings        	    (isTrueValue v)
+    | n == a_issue_errors		= withErrors        	    (isTrueValue v)
+    | n == a_remove_whitespace		= withRemoveWS        	    (isTrueValue v)
+    | n == a_preserve_comment   	= withPreserveComment 	    (isTrueValue v) 
+    | n == a_parse_by_mimetype  	= withParseByMimeType 	    (isTrueValue v)
+    | n == a_mime_types                 = withMimeTypeFile           v
+    | n == a_parse_html         	= withParseHTML       	    (isTrueValue v)
+    | n == a_validate           	= withValidate        	    (isTrueValue v)
+    | n == a_check_namespaces   	= withCheckNamespaces 	    (isTrueValue v)
+    | n == a_canonicalize       	= withCanonicalize    	    (isTrueValue v)
+    | n == a_ignore_none_xml_contents   = withIgnoreNoneXmlContents (isTrueValue v)
+    | n == a_accept_mimetypes           = withAcceptedMimeTypes     (words v)
+    | n == a_tagsoup                    = withTagSoup               (isTrueValue v)
+    | n == a_ignore_encoding_errors v_1 = withEncodingErrors        (not $ isTrueValue v)
+    | n == transferURI                  = withDefaultBaseURI         v
+    | n == a_proxy                      = withProxy                  v
+    | n == a_use_curl                   = if isTrueValue v
+                                          then withCurl
+                                          else id
+    | "curl-" `isPrefixOf` n            = withInputOption (drop 5 n) v
+    | n == a_redirect                   = withRedirect              (isTrueValue v)
+    | otherwise				= withAttr                  (addEntry n v)
+    where
+    toInt				:: Int -> String -> Int
+    toInt def s
+        | not (null s) && all isDigit s	= read s
+        | otherwise                     = def
+
+-- ------------------------------------------------------------
+
+{- |
+the main document input filter
+
+this filter can be configured by an option list, a value of type 'Text.XML.HXT.DOM.TypeDefs.Attributes'
+
+available options:
+
+* 'a_parse_html': use HTML parser, else use XML parser (default)
+
+- 'a_tagsoup' : use light weight and lazy parser based on tagsoup lib
+
+- 'a_parse_by_mimetype' : select the parser by the mime type of the document
+                          (pulled out of the HTTP header). When the mime type is set to \"text\/html\"
+                          the HTML parser (parsec or tagsoup) is taken, when it\'s set to
+                          \"text\/xml\" or \"text\/xhtml\" the XML parser (parsec or tagsoup) is taken.
+                          If the mime type is something else no further processing is performed,
+                          the contents is given back to the application in form of a single text node.
+                          If the default document encoding ('a_encoding') is set to isoLatin1, this even enables processing
+                          of arbitray binary data.
+
+- 'a_validate' : validate document againsd DTD (default), else skip validation
+
+- 'a_relax_schema' : validate document with Relax NG, the options value is the schema URI
+                     this implies using XML parser, no validation against DTD, and canonicalisation
+
+- 'a_check_namespaces' : check namespaces, else skip namespace processing (default)
+
+- 'a_canonicalize' : canonicalize document (default), else skip canonicalization
+
+- 'a_preserve_comment' : preserve comments during canonicalization, else remove comments (default)
+
+- 'a_remove_whitespace' : remove all whitespace, used for document indentation, else skip this step (default)
+
+- 'a_indent' : indent document by inserting whitespace, else skip this step (default)
+
+- 'a_issue_warnings' : issue warnings, when parsing HTML (default), else ignore HTML parser warnings
+
+- 'a_issue_errors' : issue all error messages on stderr (default), or ignore all error messages
+
+- 'a_ignore_encoding_errors': ignore all encoding errors, default is issue all encoding errors
+
+- 'a_ignore_none_xml_contents': ignore document contents of none XML\/HTML documents.
+                                This option can be useful for implementing crawler like applications, e.g. an URL checker.
+                                In those cases net traffic can be reduced.
+
+- 'a_trace' : trace level: values: 0 - 4
+
+- 'a_proxy' : proxy for http access, e.g. www-cache:3128
+
+- 'a_redirect' : automatically follow redirected URIs, default is yes
+
+- 'a_use_curl' : obsolete and ignored, HTTP acccess is always done with curl bindings for libcurl
+
+- 'a_strict_input' : file input is done strictly using the 'Data.ByteString' input functions. This ensures correct closing of files, especially when working with
+                     the tagsoup parser and not processing the whole input data. Default is off. The @ByteString@ input usually is not faster than the buildin @hGetContents@
+                     for strings.
+
+- 'a_options_curl' : deprecated but for compatibility reasons still supported.
+                     More options passed to the curl binding.
+                     Instead of using this option to set a whole bunch of options at once for curl
+                     it is recomended to use the @curl-.*@ options syntax described below.
+
+- 'a_encoding' : default document encoding ('utf8', 'isoLatin1', 'usAscii', 'iso8859_2', ... , 'iso8859_16', ...).
+                 Only XML, HTML and text documents are decoded,
+                 default decoding for XML\/HTML is utf8, for text iso latin1 (no decoding).
+                 The whole content is returned in a single text node.
+
+- 'a_mime_types' : set the mime type table for file input with given file. The format of this config file must be in the syntax of a debian linux \"mime.types\" config file
+
+- 'a_if_modified_since' : read document conditionally, only if the document is newer than the given date and time argument, the contents is delivered,
+                          else just the root node with the meta data is returned. The date and time must be given in "System.Locale.rfc822DateFormat".
+
+- curl options : the HTTP interface with libcurl can be configured with a lot of options. To support these options in an easy way, there is a naming convetion:
+                 Every option, which has the prefix @curl@ and the rest of the name forms an option as described in the curl man page, is passed to the curl binding lib.
+                 See 'Text.XML.HXT.IO.GetHTTPLibCurl.getCont' for examples. Currently most of the options concerning HTTP requests are implemented.
+
+All attributes not evaluated by readDocument are stored in the created document root node for easy access of the various
+options in e.g. the input\/output modules
+
+If the document name is the empty string or an uri of the form \"stdin:\", the document is read from standard input.
+
+examples:
+
+> readDocument [ ] "test.xml"
+
+reads and validates a document \"test.xml\", no namespace propagation, only canonicalization is performed
+
+> readDocument [ (a_validate, "0")
+>              , (a_encoding, isoLatin1)
+>              , (a_parse_by_mimetype, "1")
+>              ] "http://localhost/test.php"
+
+reads document \"test.php\", parses it as HTML or XML depending on the mimetype given from the server, but without validation, default encoding 'isoLatin1'.
+
+
+> readDocument [ (a_parse_html, "1")
+>              , (a_encoding, isoLatin1)
+>              ] ""
+
+reads a HTML document from standard input, no validation is done when parsing HTML, default encoding is 'isoLatin1',
+parsing is done with tagsoup parser, but input is read strictly
+
+> readDocument [ (a_encoding, isoLatin1)
+>              , (a_mime_type,    "/etc/mime.types")
+>              , (a_tagsoup,      "1")
+>              , (a_strict_input, "1")
+>              ] "test.svg"
+
+reads an SVG document from standard input, sets the mime type by looking in the system mimetype config file, default encoding is 'isoLatin1',
+parsing is done with the lightweight tagsoup parser, which implies no validation.
+
+> readDocument [ (a_parse_html,     "1")
+>              , (a_proxy,          "www-cache:3128")
+>              , (a_curl,           "1")
+>              , (a_issue_warnings, "0")
+>              ] "http://www.haskell.org/"
+
+reads Haskell homepage with HTML parser ignoring any warnings, with http access via external program curl and proxy \"www-cache\" at port 3128
+
+> readDocument [ (a_validate,          "1")
+>              , (a_check_namespace,   "1")
+>              , (a_remove_whitespace, "1")
+>              , (a_trace,             "2")
+>              ] "http://www.w3c.org/"
+
+read w3c home page (xhtml), validate and check namespaces, remove whitespace between tags, trace activities with level 2
+
+for minimal complete examples see 'Text.XML.HXT.Arrow.WriteDocument.writeDocument' and 'runX', the main starting point for running an XML arrow.
+-}
+
+readDocument    :: Attributes -> String -> IOStateArrow s b XmlTree
+readDocument userOptions src
+    = X.readDocument (map optionToSysConfig $ userOptions) src
+
+readFromDocument        :: Attributes -> IOStateArrow s String XmlTree
+readFromDocument userOptions
+    = applyA ( arr $ readDocument userOptions )
+
+readString      :: Attributes -> String -> IOStateArrow s b XmlTree
+readString userOptions content
+    = readDocument ( (a_encoding, unicodeString) : userOptions ) (stringProtocol ++ content)
+
+readFromString  :: Attributes -> IOStateArrow s String XmlTree
+readFromString userOptions
+    = applyA ( arr $ readString userOptions )
+
+
+-- ------------------------------------------------------------
+
+xunpickleDocument       :: PU a -> Attributes -> String -> IOStateArrow s b a
+xunpickleDocument xp al src
+                        = readDocument al src
+                          >>>
+                          traceMsg 1 ("xunpickleVal for " ++ show src ++ " started")
+                          >>>
+                          xunpickleVal xp
+                          >>>
+                          traceMsg 1 ("xunpickleVal for " ++ show src ++ " finished")
+
+-- ------------------------------------------------------------
---
+++ hxt-compat8/src/Text/XML/HXT/DOM/XmlOptions.hs
@@ -0,0 +1,188 @@
+-- |
+-- Common useful options
+--
+-- Version : $Id: XmlOptions.hs,v 1.1 2006/11/09 20:27:42 hxml Exp $
+--
+--
+
+module Text.XML.HXT.DOM.XmlOptions
+    ( inputOptions
+    , relaxOptions
+    , outputOptions
+    , generalOptions
+    , versionOptions
+    , showOptions
+
+    , selectOptions
+    , removeOptions
+    , optionIsSet
+    , isTrueValue
+    )
+where
+
+import Text.XML.HXT.DOM.XmlKeywords
+import Text.XML.HXT.DOM.TypeDefs
+
+import Data.Maybe
+import Data.String.EncodingNames
+
+import System.Console.GetOpt
+
+-- ------------------------------------------------------------
+--
+
+-- |
+-- commonly useful options for XML input
+--
+-- can be used for option definition with haskell getopt
+--
+-- defines options: 'a_trace', 'a_proxy', 'a_use_curl', 'a_do_not_use_curl', 'a_options_curl', 'a_encoding',
+-- 'a_issue_errors', 'a_do_not_issue_errors', 'a_parse_html', 'a_parse_by_mimetype', 'a_issue_warnings', 'a_do_not_issue_warnings',
+-- 'a_parse_xml', 'a_validate', 'a_do_not_validate', 'a_canonicalize', 'a_do_not_canonicalize',
+--- 'a_preserve_comment', 'a_do_not_preserve_comment', 'a_check_namespaces', 'a_do_not_check_namespaces',
+-- 'a_remove_whitespace', 'a_do_not_remove_whitespace'
+
+inputOptions    :: [OptDescr (String, String)]
+inputOptions
+    = [ Option "t"      [a_trace]                       (OptArg trc "LEVEL")                    "trace level (0-4), default 1"
+      , Option "p"      [a_proxy]                       (ReqArg (att a_proxy)         "PROXY")  "proxy for http access (e.g. \"www-cache:3128\")"
+      , Option ""       [a_redirect]                    (NoArg  (att a_redirect          v_1))  "automatically follow redirected URIs"
+      , Option ""       [a_no_redirect]                 (NoArg  (att a_redirect          v_0))  "switch off following redirected URIs"
+      , Option ""       [a_use_curl]                    (NoArg  (att a_use_curl          v_1))  "obsolete, since hxt-8.1 HTTP access is always done with curl bindings"
+      , Option ""       [a_do_not_use_curl]             (NoArg  (att a_use_curl          v_0))  "obsolete, since hxt-8.1 HTTP access is always done with curl bindings"
+      , Option ""       [a_options_curl]                (ReqArg (att a_options_curl)    "STR")  "additional curl options, e.g. for timeout, ..."
+      , Option ""       [a_default_baseuri]             (ReqArg (att transferURI)       "URI")  "default base URI, default: \"file:///<cwd>/\""
+      , Option "e"      [a_encoding]                    (ReqArg (att a_encoding)    "CHARSET")  ( "default document encoding (" ++ utf8 ++ ", " ++ isoLatin1 ++ ", " ++ usAscii ++ ", ...)" )
+      , Option ""       [a_mime_types]                  (ReqArg (att a_mime_types)     "FILE")  "set mime type configuration file, e.g. \"/etc/mime.types\""
+      , Option ""       [a_issue_errors]                (NoArg  (att a_issue_errors      v_1))  "issue all errorr messages on stderr (default)"
+      , Option ""       [a_do_not_issue_errors]         (NoArg  (att a_issue_errors      v_0))  "ignore all error messages"
+      , Option ""       [a_ignore_encoding_errors]      (NoArg  (att a_ignore_encoding_errors v_1))   "ignore encoding errors"
+      , Option ""       [a_ignore_none_xml_contents]    (NoArg  (att a_ignore_none_xml_contents v_1)) "discards all contents of none XML/HTML documents, only the meta info remains in the doc tree"
+      , Option ""       [a_accept_mimetypes]            (ReqArg (att a_accept_mimetypes) "MIMETYPES") "only accept documents matching the given list of mimetype specs"
+      , Option "H"      [a_parse_html]                  (NoArg  (att a_parse_html        v_1))  "parse input as HTML, try to interprete everything as HTML, no validation"
+      , Option "M"      [a_parse_by_mimetype]           (NoArg  (att a_parse_by_mimetype v_1))  "parse dependent on mime type: text/html as HTML, text/xml and text/xhtml and others as XML, else no parse"
+      , Option ""       [a_parse_xml]                   (NoArg  (att a_parse_html        v_0))  "parse input as XML, (default)"
+      , Option ""       [a_strict_input]                (NoArg  (att a_strict_input      v_1))  "read input files strictly, this ensures closing the files correctly even if not read completely"
+      , Option ""       [a_issue_warnings]              (NoArg  (att a_issue_warnings    v_1))  "issue warnings, when parsing HTML (default)"
+      , Option "Q"      [a_do_not_issue_warnings]       (NoArg  (att a_issue_warnings    v_0))  "ignore warnings, when parsing HTML"
+      , Option ""       [a_validate]                    (NoArg  (att a_validate          v_1))  "document validation when parsing XML (default)"
+      , Option "w"      [a_do_not_validate]             (NoArg  (att a_validate          v_0))  "only wellformed check, no validation"
+      , Option ""       [a_canonicalize]                (NoArg  (att a_canonicalize      v_1))  "canonicalize document, remove DTD, comment, transform CDATA, CharRef's, ... (default)"
+      , Option "c"      [a_do_not_canonicalize]         (NoArg  (att a_canonicalize      v_0))  "do not canonicalize document, don't remove DTD, comment, don't transform CDATA, CharRef's, ..."
+      , Option "C"      [a_preserve_comment]            (NoArg  (att a_preserve_comment  v_1))  "don't remove comments during canonicalisation"
+      , Option ""       [a_do_not_preserve_comment]     (NoArg  (att a_preserve_comment  v_0))  "remove comments during canonicalisation (default)"
+      , Option "n"      [a_check_namespaces]            (NoArg  (att a_check_namespaces  v_1))  "tag tree with namespace information and check namespaces"
+      , Option ""       [a_do_not_check_namespaces]     (NoArg  (att a_check_namespaces  v_0))  "ignore namespaces (default)"
+      , Option "r"      [a_remove_whitespace]           (NoArg  (att a_remove_whitespace v_1))  "remove redundant whitespace, simplifies tree and processing"
+      , Option ""       [a_do_not_remove_whitespace]    (NoArg  (att a_remove_whitespace v_0))  "don't remove redundant whitespace (default)"
+      ]
+    where
+    att n v     = (n, v)
+    trc = att a_trace . show . max 0 . min 9 . (read :: String -> Int) . ('0':) . filter (`elem` "0123456789") . fromMaybe v_1
+
+
+-- | available Relax NG validation options
+--
+-- defines options
+-- 'a_check_restrictions', 'a_validate_externalRef', 'a_validate_include', 'a_do_not_check_restrictions',
+-- 'a_do_not_validate_externalRef', 'a_do_not_validate_include'
+
+relaxOptions :: [OptDescr (String, String)]
+relaxOptions
+    = [ Option "X" [a_relax_schema]                     (ReqArg (att a_relax_schema) "SCHEMA")  "validation with Relax NG, SCHEMA is the URI for the Relax NG schema"
+      , Option ""  [a_check_restrictions]               (NoArg (a_check_restrictions,    v_1))  "check Relax NG schema restrictions during schema simplification (default)"
+      , Option ""  [a_do_not_check_restrictions]        (NoArg (a_check_restrictions,    v_0))  "do not check Relax NG schema restrictions"
+      , Option ""  [a_validate_externalRef]             (NoArg (a_validate_externalRef,  v_1))  "validate a Relax NG schema referenced by a externalRef-Pattern (default)"
+      , Option ""  [a_do_not_validate_externalRef]      (NoArg (a_validate_externalRef,  v_0))  "do not validate a Relax NG schema referenced by an externalRef-Pattern"
+      , Option ""  [a_validate_include]                 (NoArg (a_validate_include,      v_1))  "validate a Relax NG schema referenced by an include-Pattern (default)"
+      , Option ""  [a_do_not_validate_include]          (NoArg (a_validate_include,      v_0))   "do not validate a Relax NG schema referenced by an include-Pattern"
+        {-
+      , Option ""  [a_output_changes]                   (NoArg (a_output_changes,        v_1))  "output Pattern transformations in case of an error"
+      , Option ""  [a_do_not_collect_errors]            (NoArg (a_do_not_collect_errors, v_1))  "stop Relax NG simplification after the first error has occurred"
+        -}
+      ]
+    where
+    att n v     = (n, v)
+
+-- |
+-- commonly useful options for XML output
+--
+-- defines options: 'a_indent', 'a_output_encoding', 'a_output_file', 'a_output_html'
+
+outputOptions   :: [OptDescr (String, String)]
+outputOptions
+    = [ Option "i"      [a_indent]              (NoArg  (att a_indent                v_1))      "indent XML output for readability"
+      , Option "o"      [a_output_encoding]     (ReqArg (att a_output_encoding) "CHARSET")      ( "encoding of output (" ++ utf8 ++ ", " ++ isoLatin1 ++ ", " ++ usAscii ++ ")" )
+      , Option "f"      [a_output_file]         (ReqArg (att a_output_file)        "FILE")      "output file for resulting document (default: stdout)"
+      , Option ""       [a_output_html]         (NoArg  (att a_output_html           v_1))      "output of none ASCII chars as HTMl entity references"
+      , Option ""       [a_no_xml_pi]           (NoArg  (att a_no_xml_pi             v_1))      ("output without <?xml ...?> processing instruction, useful in combination with --" ++ show a_output_html)
+      , Option ""       [a_output_xhtml]        (NoArg  (att a_output_xhtml          v_1))      "output of HTML elements with empty content (script, ...) done in format <elem...></elem> instead of <elem/>"
+      , Option ""       [a_no_empty_elem_for]   (ReqArg (att a_no_empty_elem_for) "NAMES")      "output of empty elements done in format <elem...></elem> only for given list of element names"
+      , Option ""       [a_no_empty_elements]   (NoArg  (att a_no_empty_elements     v_1))      "output of empty elements done in format <elem...></elem> instead of <elem/>"
+      , Option ""       [a_add_default_dtd]     (NoArg  (att a_add_default_dtd       v_1))      "add the document type declaration given in the input document"
+      , Option ""       [a_text_mode]           (NoArg  (att a_text_mode             v_1))      "output in text mode"
+      ]
+    where
+    att n v     = (n, v)
+
+-- |
+-- commonly useful options
+--
+-- defines options: 'a_verbose', 'a_help'
+
+generalOptions  :: [OptDescr (String, String)]
+generalOptions
+    = [ Option "v"      [a_verbose]             (NoArg  (a_verbose, v_1))               "verbose output"
+      , Option "h?"     [a_help]                (NoArg  (a_help,    v_1))               "this message"
+      ]
+
+-- |
+-- defines 'a_version' option
+
+versionOptions  :: [OptDescr (String, String)]
+versionOptions
+    = [ Option "V"      [a_version]             (NoArg  (a_version, v_1))               "show program version"
+      ]
+
+-- |
+-- debug output options
+
+showOptions     :: [OptDescr (String, String)]
+showOptions
+    = [ Option ""       [a_show_tree]           (NoArg  (a_show_tree,    v_1))          "output tree representation instead of document source"
+      , Option ""       [a_show_haskell]        (NoArg  (a_show_haskell, v_1))          "output internal Haskell representation instead of document source"
+      ]
+
+-- ------------------------------------------------------------
+
+-- |
+-- select options from a predefined list of option desciptions
+
+selectOptions   :: [String] -> [OptDescr (String, String)] -> [OptDescr (String, String)]
+selectOptions ol os
+    = concat . map (\ on -> filter (\ (Option _ ons _ _) -> on `elem` ons) os) $ ol
+
+removeOptions   :: [String] -> [OptDescr (String, String)] -> [OptDescr (String, String)]
+removeOptions ol os
+    = filter (\ (Option _ ons _ _) -> not . any (`elem` ol) $ ons ) os
+
+-- |
+-- check whether an option is set
+--
+-- reads the value of an attribute, usually applied to a document root node,
+-- and checks if the value represents True. The following strings are interpreted
+-- as true: \"1\", \"True\", \"true\", \"yes\", \"Yes\".
+
+optionIsSet     :: String -> Attributes -> Bool
+optionIsSet n   = isTrueValue . lookupDef "" n
+
+-- | check whether a string represents True
+--
+-- definition:
+--
+-- > isTrueValue        = (`elem` ["1", "True", "true", "Yes", "yes"])
+
+isTrueValue     :: String -> Bool
+isTrueValue     = (`elem` ["1", "True", "true", "Yes", "yes"])
+
+-- ------------------------------------------------------------
