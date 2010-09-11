1ecd47ee43132f98c740e5b59a2e52f9a055c7d3
Theo Lingen <theo@sichel.welt.all>
2010-09-11 16:26:39 +0200

    conditional configuration of hxt-install.cabal added

--- hxt-install/HXTTest.hs
+++ hxt-install/HXTTest.hs
@@ -2,13 +2,19 @@ module Main
 where
 
 import Text.XML.HXT.Core
-import Text.XML.HXT.Curl
 import Text.XML.HXT.HTTP
-import Text.XML.HXT.TagSoup
 import Text.XML.HXT.RelaxNG
 import Text.XML.HXT.XPath
 import Text.XML.HXT.XSLT
 import Text.XML.HXT.Cache
+
+#ifdef curl
+import Text.XML.HXT.Curl
+#endif
+
+#ifdef tagsoup
+import Text.XML.HXT.TagSoup
+#endif
 
 -- ----------------------------------------------------------
 
@@ -22,47 +28,47 @@ import Test.HUnit
 
 -- ----------------------------------------------------------
 
-example1	:: (String, String)
-example1	= ( "example1.xml"
+example1        :: (String, String)
+example1        = ( "example1.xml"
                   , unlines $
                     [ "<?xml version='1.0'?>"
                     , "<html> <head> </head> <body> </body> </html>"
                     ]
                   )
 
-example2	:: (String, String)
-example2	= ( "example2.xml"
+example2        :: (String, String)
+example2        = ( "example2.xml"
                   , unlines $
                     [ "<?xml version='1.0'?>"
                     , "<html><head/><body/></html>"
                     ]
                   )
 
-example3	:: (String, String)
-example3	= ( "example3.xml"
+example3        :: (String, String)
+example3        = ( "example3.xml"
                   , unlines $
                     [ "<?xml version='1.0'?>"
                     , "<html>&auml;</html>"
                     ]
                   )
 
-example4	:: (String, String)
-example4	= ( "example4.html"
+example4        :: (String, String)
+example4        = ( "example4.html"
                   , unlines $
                     [ "<html>&auml;</html>"
                     ]
                   )
 
-example5	:: (String, String)
-example5	= ( "example5.xml"
+example5        :: (String, String)
+example5        = ( "example5.xml"
                   , unlines $
                     [ "<?xml version='1.0'?>"
                     , "<html><!-- xxx --></html>"
                     ]
                   )
 
-example6	:: (String, String)
-example6	= ( "example6.xml"
+example6        :: (String, String)
+example6        = ( "example6.xml"
                   , unlines $
                     [ "<?xml version='1.0'?>"
                     , "<!DOCTYPE a ["
@@ -72,8 +78,8 @@ example6	= ( "example6.xml"
                     ]
                   )
 
-example7	:: (String, String)
-example7	= ( "example7.xml"
+example7        :: (String, String)
+example7        = ( "example7.xml"
                   , unlines $
                     [ "<?xml version='1.0'?>"
                     , "<!DOCTYPE a ["
@@ -83,37 +89,37 @@ example7	= ( "example7.xml"
                     ]
                   )
 
-example8	:: (String, String)
-example8	= ( "example8.xml"
+example8        :: (String, String)
+example8        = ( "example8.xml"
                   , unlines $
                     [ "<?xml version='1.0'?>"
                     , "<n:a xmlns:n='nnn'/>"
                     ]
                   )
 
-example9	:: (String, String)
-example9	= ( "example9.xml"
+example9        :: (String, String)
+example9        = ( "example9.xml"
                   , unlines $
                     [ "<?xml version='1.0'?>"
                     , "<n:a xmlns:m='nnn'/>"
                     ]
                   )
 
-example10	:: (String, String)
-example10	= ( "example10.xml"
+example10       :: (String, String)
+example10       = ( "example10.xml"
                   , unlines $
                     [ "<a><![CDATA[x]]></a>"
                     ]
                   )
 
-example11	:: (String, String)
-example11	= ( "example11.txt"
+example11       :: (String, String)
+example11       = ( "example11.txt"
                   , unlines $
                     [ "hello"
                     ]
                   )
 
-examples	:: [(String, String)]
+examples        :: [(String, String)]
 examples        = [ example1
                   , example2
                   , example3
@@ -127,19 +133,19 @@ examples        = [ example1
                   , example11
                   ]
 
-genInputFile	:: (String, String) -> IO ()
+genInputFile    :: (String, String) -> IO ()
 genInputFile f  = do
                   remInputFile (fst f)
                   uncurry writeFile f
 
-remInputFile	:: String -> IO ()
+remInputFile    :: String -> IO ()
 remInputFile f  = do
                   e <- doesFileExist f
                   if e
                      then removeFile f
                      else return ()
 
-genInputFiles	:: IO ()
+genInputFiles   :: IO ()
 genInputFiles   = sequence_ $ map genInputFile $ examples
 
 remInputFiles   :: IO ()
@@ -147,7 +153,7 @@ remInputFiles   = sequence_ $ map (remInputFile . fst) $ examples
 
 -- ----------------------------------------------------------
 
-configParseTests	:: Test
+configParseTests        :: Test
 configParseTests
     = TestLabel "Test simple parser configurations" $
       TestList $
@@ -179,23 +185,23 @@ configParseTests
 
       , ( "example3.xml"
         , []
-        , ""	                -- parser error in &auml;
+        , ""                    -- parser error in &auml;
         )
       , ( "example4.html"
         , [withParseHTML yes]
-        , "<html>\228</html>"	-- no parse error
+        , "<html>\228</html>"   -- no parse error
         )
       , ( "example4.html"
         , [withParseHTML no]
-        , ""			-- parse XML: error
+        , ""                    -- parse XML: error
         )
       , ( "example3.xml"
         , [withParseByMimeType yes]
-        , ""	                -- parser error in &auml;
+        , ""                    -- parser error in &auml;
         )
       , ( "example4.html"
         , [withParseByMimeType yes]
-        , "<html>\228</html>"	-- no parse error
+        , "<html>\228</html>"   -- no parse error
         )
 
 
@@ -212,7 +218,7 @@ configParseTests
         , "<html><!-- xxx --></html>"
         )
 
-      , ( "example6.xml"		-- withValidate
+      , ( "example6.xml"                -- withValidate
         , []
         , "<a/>"
         )
@@ -237,7 +243,7 @@ configParseTests
         , "<a>x</a>"
         )
 
-      , ( "example8.xml"		-- namespace check
+      , ( "example8.xml"                -- namespace check
         , []
         , "<n:a xmlns:n=\"nnn\"/>"
         )
@@ -275,7 +281,7 @@ configParseTests
         , "<a><![CDATA[x]]></a>\n"
         )
 
-      , ( "example11.txt"		-- parse by mimetype and none XML/HTML contents
+      , ( "example11.txt"               -- parse by mimetype and none XML/HTML contents
         , []
         , ""
         )
@@ -302,12 +308,14 @@ configParseTests
 
 -- ----------------------------------------------------------
 
-configTagSoupTests	:: Test
+#ifdef tagsoup
+configTagSoupTests      :: Test
 configTagSoupTests
     = TestLabel "Test TagSoup parser configurations" $
       TestList $
       map mkTest $
-      [ ( "example2.xml"
+      [
+        ( "example2.xml"
         , []
         , "<html><head/><body/></html>"
         )
@@ -330,7 +338,7 @@ configTagSoupTests
         )
 
 
-      , ( "example8.xml"		-- namespace check
+      , ( "example8.xml"                -- namespace check
         , []
         , "<n:a xmlns:n=\"nnn\"/>"
         )
@@ -352,7 +360,7 @@ configTagSoupTests
         )
       , ( "example9.xml"
         , [withCheckNamespaces yes]
-        , "xxxxxxxxxxxxxxx"
+        , ""
         )
 
       , ( "example10.xml"
@@ -363,18 +371,18 @@ configTagSoupTests
         , [withCanonicalize yes]
         , "<a>x</a>"
         )
-      , ( "example10.xml"		-- tagsoup removes CDATAs
+      , ( "example10.xml"               -- tagsoup removes CDATAs
         , [withParseHTML yes, withCanonicalize no]
-        , "<a>x</a>\n"
+        , "<a>x</a>"
         )
 
-      , ( "example11.txt"		-- parse by mimetype and none XML/HTML contents
+      , ( "example11.txt"               -- parse by mimetype and none XML/HTML contents
         , []
         , ""
         )
       , ( "example11.txt"
         , [withParseByMimeType yes]
-        , "hello\n"
+        , "hello\n"			-- TODO parse by mimetype does not work together with tagsoup
         )
       , ( "example11.txt"
         , [withIgnoreNoneXmlContents yes, withParseByMimeType yes]
@@ -392,10 +400,14 @@ configTagSoupTests
                  >>>
                  writeDocumentToString [withRemoveWS no]
           assertEqual "TagSoup parser results differ" exp (concat res)
+#else
+configTagSoupTests      :: Test
+configTagSoupTests      = TestList []
+#endif
 
 -- ----------------------------------------------------------
 
-allTests	:: Test
+allTests        :: Test
 allTests
     = TestList
       [ TestLabel "Generate test input files" $
@@ -408,17 +420,17 @@ allTests
         TestCase remInputFiles
       ]
 
-main	:: IO ()
+main    :: IO ()
 main
     = do
       genInputFiles
       c <- runTestTT allTests
       putStrLn $ show c
       let errs = errors c
-	  fails = failures c
+          fails = failures c
       exitWith (codeGet errs fails)
 
-codeGet	:: Int -> Int -> ExitCode
+codeGet :: Int -> Int -> ExitCode
 codeGet errs fails
     | fails > 0       = ExitFailure 2
     | errs > 0        = ExitFailure 1
