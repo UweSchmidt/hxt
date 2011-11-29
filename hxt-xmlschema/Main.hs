994a6210e71449271a2d3c1face95b017bdb7ce5
Thorben <thorbenguelck@gmx.net>
2011-11-29 11:54:06 +0100

    extended examples

--- hxt-xmlschema/Main.hs
+++ hxt-xmlschema/Main.hs
@@ -3,19 +3,19 @@ import Text.XML.HXT.Core
 import Data.Map (Map, fromList, toList, keys)
 
 data XmlSchema = XmlSchema
-  { sElements     :: Elements
-  , sSimpleTypes  :: SimpleTypes
+  { sSimpleTypes  :: SimpleTypes
   , sComplexTypes :: ComplexTypes
+  , sElements     :: Elements
   }
   deriving (Show, Eq)
 
-type Elements     = Map String Element
 type SimpleTypes  = Map String SimpleType
 type ComplexTypes = Map String ComplexType
+type Elements     = Map String Element
 
-type Element      = XmlTree
 type SimpleType   = XmlTree
 type ComplexType  = XmlTree
+type Element      = XmlTree
 
 instance XmlPickler XmlSchema where
   xpickle = xpXmlSchema
@@ -25,17 +25,8 @@ xpXmlSchema :: PU XmlSchema
 xpXmlSchema
   = xpElem "xs:schema" $
     xpWrap ( uncurry3 XmlSchema
-           , \ s -> (sElements s, sSimpleTypes s, sComplexTypes s) ) $
-    xpTriple xpElements xpSimpleTypes xpComplexTypes
-
-xpElements :: PU Elements
-xpElements
-  = xpWrap ( fromList
-           , toList
-           ) $
-    xpList $
-    xpElem "xs:element" $
-    xpPair (xpAttr "name" xpText) xpTree
+           , \ s -> (sSimpleTypes s, sComplexTypes s, sElements s) ) $
+    xpTriple xpSimpleTypes xpComplexTypes xpElements
 
 xpSimpleTypes :: PU SimpleTypes
 xpSimpleTypes
@@ -53,6 +44,15 @@ xpComplexTypes
            ) $
     xpList $
     xpElem "xs:complexType" $
+    xpPair (xpAttr "name" xpText) xpTree
+
+xpElements :: PU Elements
+xpElements
+  = xpWrap ( fromList
+           , toList
+           ) $
+    xpList $
+    xpElem "xs:element" $
     xpPair (xpAttr "name" xpText) xpTree
 
 loadXmlSchema :: IO XmlSchema
@@ -83,8 +83,9 @@ main :: IO ()
 main
   = do
     xmlschema <- loadXmlSchema
+    putStrLn $ concat $ map (\ s -> s ++ " ") $ keys (sSimpleTypes xmlschema)
+    putStrLn $ concat $ map (\ s -> s ++ " ") $ keys (sComplexTypes xmlschema)
     putStrLn $ concat $ map (\ s -> s ++ " ") $ keys (sElements xmlschema)
     storeXmlSchema xmlschema
     return ()
-
 
