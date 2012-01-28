3c234f3054096b48be27d4cee71b92aefc800dfe
Thorben <thorbenguelck@gmx.net>
2012-01-28 14:36:41 +0100

    added NS pickling, missing attributes, changed global element pickling

--- hxt-xmlschema/Main.hs
+++ hxt-xmlschema/Main.hs
@@ -1,11 +1,10 @@
-{- LANGUAGE TypeSynonymInstances #-}
-
 import Text.XML.HXT.Core
 
-import Data.Map (Map, toList, empty, insert)
+import Data.Map (Map, elems, toList, empty, insert)
 
 data XmlSchema      = XmlSchema
-                    { sSimpleTypes  :: SimpleTypes
+                    { sTargetNS     :: Maybe String
+                    , sSimpleTypes  :: SimpleTypes
                     , sComplexTypes :: ComplexTypes
                     , sElements     :: Elements
                     , sIncludes     :: Includes
@@ -18,10 +17,13 @@ type Elements       = Map Name Element
 type Includes       = [Include]
 type Name           = String
 
-type XmlSchema'     = [XmlSchemaPart]
+data XmlSchema'     = XmlSchema'
+                    { targetNS :: Maybe String
+                    , parts    :: [XmlSchemaPart]
+                    }
 data XmlSchemaPart  = St {unSt :: (Name, SimpleType)}
                     | Ct {unCt :: (Name, ComplexType)}
-                    | El {unEl :: (Name, Element)}
+                    | El {unEl :: Element}
                     | In {unIn :: Include}
 
 data SimpleType     = Restr {unRestr :: Restriction}
@@ -50,7 +52,11 @@ type ItemType       = String
 type Union          = MemberTypes
 type MemberTypes    = String
 
-type ComplexType    = [CTElems]
+data ComplexType    = ComplexType
+                    { mixed   :: Maybe String
+                    , ctelems :: [CTElems]
+                    }
+                    deriving (Show, Eq)
 data CTElems        = Sq    {unSq    :: Sequence}
                     | Ch    {unCh    :: Choice}
                     | Al    {unAl    :: All}
@@ -83,10 +89,24 @@ data ElTypeDef      = TRef  {unTRef  :: Name}
                     | CTDef {unCTDef :: ComplexType}
                     deriving (Show, Eq)
 
-data Include        = Incl {unIncl :: String}
-                    | Imp  {unImp  :: String}
+data Include        = Incl  {unIncl  :: String}
+                    | Imp   {unImp   :: (String, String)}
                     | Redef {unRedef :: (String, XmlTrees)}
                     deriving (Show, Eq)
+
+-- Namespace handling
+
+nsPrefix :: String
+nsPrefix = "xs"
+
+nsUri :: String
+nsUri    = "http://www.w3.org/2001/XMLSchema"
+
+xpElem' :: String -> PU a -> PU a
+xpElem' = xpElemNS nsUri nsPrefix
+
+xpAttr' :: String -> PU a -> PU a
+xpAttr' = xpAttrNS nsUri nsPrefix
 
 -- Conversion between Schema and Schema'
 
@@ -94,30 +114,29 @@ toSchema :: XmlSchema' -> XmlSchema
 toSchema s
   = toSchemaRec s empty empty empty []
     where
-    toSchemaRec []                sts cts els ins = XmlSchema { sSimpleTypes  = sts
-                                                              , sComplexTypes = cts
-                                                              , sElements     = els
-                                                              , sIncludes     = ins
-                                                              }
-    toSchemaRec ((St (k, st)):xs) sts cts els ins = toSchemaRec xs (insert k st sts) cts els ins
-    toSchemaRec ((Ct (k, ct)):xs) sts cts els ins = toSchemaRec xs sts (insert k ct cts) els ins
-    toSchemaRec ((El (k, el)):xs) sts cts els ins = toSchemaRec xs sts cts (insert k el els) ins
-    toSchemaRec ((In incl)   :xs) sts cts els ins = toSchemaRec xs sts cts els (ins ++ [incl]) -- keep ordering
+    toSchemaRec (XmlSchema' tns [])                sts cts els ins = XmlSchema tns sts cts els ins
+    toSchemaRec (XmlSchema' tns ((St (k, st)):xs)) sts cts els ins = toSchemaRec (XmlSchema' tns xs) (insert k st sts) cts els ins
+    toSchemaRec (XmlSchema' tns ((Ct (k, ct)):xs)) sts cts els ins = toSchemaRec (XmlSchema' tns xs) sts (insert k ct cts) els ins
+    toSchemaRec (XmlSchema' tns ((El el)     :xs)) sts cts els ins = toSchemaRec (XmlSchema' tns xs) sts cts (insert (name (unElDef el)) el els) ins
+    toSchemaRec (XmlSchema' tns ((In incl)   :xs)) sts cts els ins = toSchemaRec (XmlSchema' tns xs) sts cts els (ins ++ [incl]) -- keep ordering
 
 fromSchema :: XmlSchema -> XmlSchema'
 fromSchema s
-  = concat [sts, cts, els, ins]
+  = XmlSchema' (sTargetNS s) $ concat [sts, cts, els, ins]
     where 
     sts = map St $ toList $ sSimpleTypes s
     cts = map Ct $ toList $ sComplexTypes s
-    els = map El $ toList $ sElements s
+    els = map El $ elems  $ sElements s
     ins = map In $ sIncludes s
 
 -- Pickler definitions
 
 xpXmlSchema' :: PU XmlSchema'
 xpXmlSchema'
-  = xpElem "xs:schema" $ xpList $ xpSchemaPart
+  = xpElem' "schema" $ xpAddNSDecl nsPrefix nsUri $ 
+    xpWrap (\ (a, b) -> XmlSchema' a b , \ t -> (targetNS t, parts t)) $
+    xpPair (xpOption $ xpAttr' "targetNamespace" xpText) $
+    xpList $ xpSchemaPart
 
 xpSchemaPart :: PU XmlSchemaPart
 xpSchemaPart
@@ -127,9 +146,9 @@ xpSchemaPart
     tag (Ct _) = 1
     tag (El _) = 2
     tag (In _) = 3
-    ps = [ xpWrap (St, unSt) $ xpElem "xs:simpleType"  $ xpPair (xpAttr "name" xpText) xpSimpleType
-         , xpWrap (Ct, unCt) $ xpElem "xs:complexType" $ xpPair (xpAttr "name" xpText) xpComplexType
-         , xpWrap (El, unEl) $ xpElem "xs:element"     $ xpPair (xpAttr "name" xpText) xpElement
+    ps = [ xpWrap (St, unSt) $ xpElem' "simpleType"  $ xpPair (xpAttr' "name" xpText) xpSimpleType
+         , xpWrap (Ct, unCt) $ xpElem' "complexType" $ xpPair (xpAttr' "name" xpText) xpComplexType
+         , xpWrap (El, unEl) $ xpElem' "element"     $ xpElement
          , xpWrap (In, unIn) $ xpInclude
          ]
 
@@ -140,13 +159,13 @@ xpSimpleType
     tag (Restr _) = 0
     tag (Lst _)   = 1
     tag (Un _)    = 2
-    ps = [ xpWrap (Restr, unRestr) $ xpElem "xs:restriction" $ xpRestriction
-         , xpWrap (Lst,   unLst)   $ xpElem "xs:list"        $ xpAttr "itemType" xpText
-         , xpWrap (Un,    unUn)    $ xpElem "xs:union"       $ xpAttr "memberTypes" xpText
+    ps = [ xpWrap (Restr, unRestr) $ xpElem' "restriction" $ xpRestriction
+         , xpWrap (Lst,   unLst)   $ xpElem' "list"        $ xpAttr' "itemType" xpText
+         , xpWrap (Un,    unUn)    $ xpElem' "union"       $ xpAttr' "memberTypes" xpText
          ]
 xpRestriction :: PU Restriction
 xpRestriction
-  = xpPair (xpAttr "base" xpText) $ xpList $ xpRestrAttr
+  = xpPair (xpAttr' "base" xpText) $ xpList $ xpRestrAttr
 xpRestrAttr :: PU RestrAttr
 xpRestrAttr
   = xpAlt tag ps
@@ -162,22 +181,24 @@ xpRestrAttr
     tag (MinLength _)      = 8
     tag (MaxLength _)      = 9
     tag (Length _)         = 10
-    ps = [ xpWrap (MinIncl,        unMinIncl)        $ xpElem "xs:minInclusive"   $ xpAttr "value" xpText
-         , xpWrap (MaxIncl,        unMaxIncl)        $ xpElem "xs:maxInclusive"   $ xpAttr "value" xpText
-         , xpWrap (MinExcl,        unMinExcl)        $ xpElem "xs:minExclusive"   $ xpAttr "value" xpText
-         , xpWrap (MaxExcl,        unMaxExcl)        $ xpElem "xs:maxExclusive"   $ xpAttr "value" xpText
-         , xpWrap (TotalDigits,    unTotalDigits)    $ xpElem "xs:totalDigits"    $ xpAttr "value" xpText
-         , xpWrap (FractionDigits, unFractionDigits) $ xpElem "xs:fractionDigits" $ xpAttr "value" xpText
-         , xpWrap (Enumeration,    unEnumeration)    $ xpElem "xs:enumeration"    $ xpAttr "value" xpText
-         , xpWrap (Pattern,        unPattern)        $ xpElem "xs:pattern"        $ xpAttr "value" xpText
-         , xpWrap (MinLength,      unMinLength)      $ xpElem "xs:minLength"      $ xpAttr "value" xpText
-         , xpWrap (MaxLength,      unMaxLength)      $ xpElem "xs:maxLength"      $ xpAttr "value" xpText
-         , xpWrap (Length,         unLength)         $ xpElem "xs:length"         $ xpAttr "value" xpText
+    ps = [ xpWrap (MinIncl,        unMinIncl)        $ xpElem' "minInclusive"   $ xpAttr' "value" xpText
+         , xpWrap (MaxIncl,        unMaxIncl)        $ xpElem' "maxInclusive"   $ xpAttr' "value" xpText
+         , xpWrap (MinExcl,        unMinExcl)        $ xpElem' "minExclusive"   $ xpAttr' "value" xpText
+         , xpWrap (MaxExcl,        unMaxExcl)        $ xpElem' "maxExclusive"   $ xpAttr' "value" xpText
+         , xpWrap (TotalDigits,    unTotalDigits)    $ xpElem' "totalDigits"    $ xpAttr' "value" xpText
+         , xpWrap (FractionDigits, unFractionDigits) $ xpElem' "fractionDigits" $ xpAttr' "value" xpText
+         , xpWrap (Enumeration,    unEnumeration)    $ xpElem' "enumeration"    $ xpAttr' "value" xpText
+         , xpWrap (Pattern,        unPattern)        $ xpElem' "pattern"        $ xpAttr' "value" xpText
+         , xpWrap (MinLength,      unMinLength)      $ xpElem' "minLength"      $ xpAttr' "value" xpText
+         , xpWrap (MaxLength,      unMaxLength)      $ xpElem' "maxLength"      $ xpAttr' "value" xpText
+         , xpWrap (Length,         unLength)         $ xpElem' "length"         $ xpAttr' "value" xpText
          ]
 
 xpComplexType :: PU ComplexType
 xpComplexType
-  = xpList $ 
+  = xpWrap (\ (a, b) -> ComplexType a b , \ t -> (mixed t, ctelems t)) $
+    xpPair (xpOption $ xpAttr' "mixed" xpText) $
+    xpList $ 
     xpAlt tag ps
     where
     tag (Sq _)    = 0
@@ -185,23 +206,23 @@ xpComplexType
     tag (Al _)    = 2
     tag (Attr _)  = 3
     tag (CCont _) = 4
-    ps = [ xpWrap (Sq,    unSq)    $ xpElem "xs:sequence"       $ xpSequence
-         , xpWrap (Ch,    unCh)    $ xpElem "xs:choice"         $ xpList $ xpElem "xs:element" $ xpElement
-         , xpWrap (Al,    unAl)    $ xpElem "xs:all"            $ xpList $ xpElem "xs:element" $ xpElement
-         , xpWrap (Attr,  unAttr)  $ xpElem "xs:attribute"      $ xpPair (xpAttr "name" xpText) (xpAttr "type" xpText)
-         , xpWrap (CCont, unCCont) $ xpElem "xs:complexContent" $ xpComplexContent
+    ps = [ xpWrap (Sq,    unSq)    $ xpElem' "sequence"       $ xpSequence
+         , xpWrap (Ch,    unCh)    $ xpElem' "choice"         $ xpList $ xpElem' "element" $ xpElement
+         , xpWrap (Al,    unAl)    $ xpElem' "all"            $ xpList $ xpElem' "element" $ xpElement
+         , xpWrap (Attr,  unAttr)  $ xpElem' "attribute"      $ xpPair (xpAttr' "name" xpText) (xpAttr' "type" xpText)
+         , xpWrap (CCont, unCCont) $ xpElem' "complexContent" $ xpComplexContent
          ]
 xpSequence :: PU Sequence
 xpSequence
-  = xpList $ xpElem "xs:element" $ xpElement
+  = xpList $ xpElem' "element" $ xpElement
 xpComplexContent :: PU ComplexContent
 xpComplexContent
   = xpAlt tag ps
     where
     tag (CCExt _)   = 0
     tag (CCRestr _) = 1
-    ps = [ xpWrap (CCExt,   unCCExt)   $ xpElem "xs:extension"   $ xpPair (xpAttr "base" xpText) $ xpElem "xs:sequence" $ xpSequence
-         , xpWrap (CCRestr, unCCRestr) $ xpElem "xs:restriction" $ xpPair (xpAttr "base" xpText) $ xpElem "xs:sequence" $ xpSequence
+    ps = [ xpWrap (CCExt,   unCCExt)   $ xpElem' "extension"   $ xpPair (xpAttr' "base" xpText) $ xpElem' "sequence" $ xpSequence
+         , xpWrap (CCRestr, unCCRestr) $ xpElem' "restriction" $ xpPair (xpAttr' "base" xpText) $ xpElem' "sequence" $ xpSequence
          ]
 
 xpElement :: PU Element
@@ -210,21 +231,21 @@ xpElement
     where
     tag (ElRef _) = 0
     tag (ElDef _) = 1
-    ps = [ xpWrap (ElRef, unElRef) $ xpAttr "ref" xpText
+    ps = [ xpWrap (ElRef, unElRef) $ xpAttr' "ref" xpText
          , xpWrap (ElDef, unElDef) $ xpElementDef
          ]
 xpElementDef :: PU ElementDef
 xpElementDef
   = xpWrap (\ (a, b, c, d) -> ElementDef a b c d, \ t -> (name t, elTypeDef t, minOcc t, maxOcc t)) $
-    xp4Tuple (xpAttr "name" xpText) (xpElTypeDef) (xpOption $ xpAttr "minOccurs" xpText) (xpOption $ xpAttr "maxOccurs" xpText)
+    xp4Tuple (xpAttr' "name" xpText) (xpElTypeDef) (xpOption $ xpAttr' "minOccurs" xpText) (xpOption $ xpAttr' "maxOccurs" xpText)
 xpElTypeDef :: PU ElTypeDef
 xpElTypeDef
   = xpAlt tag ps
     where
     tag (TRef _)  = 0
     tag (CTDef _) = 1
-    ps = [ xpWrap (TRef,  unTRef)  $ xpAttr "type" xpText
-         , xpWrap (CTDef, unCTDef) $ xpElem "xs:complexType" $ xpComplexType
+    ps = [ xpWrap (TRef,  unTRef)  $ xpAttr' "type" xpText
+         , xpWrap (CTDef, unCTDef) $ xpElem' "complexType" $ xpComplexType
          ]
 
 xpInclude :: PU Include
@@ -234,9 +255,9 @@ xpInclude
     tag (Incl _)   = 0
     tag (Imp _)    = 1
     tag (Redef _)  = 2
-    ps = [ xpElem "xs:include"  $ xpWrap (Incl,  unIncl)  $ xpAttr "schemaLocation" xpText
-         , xpElem "xs:import"   $ xpWrap (Imp,   unImp)   $ xpAttr "namespace" xpText
-         , xpElem "xs:redefine" $ xpWrap (Redef, unRedef) $ xpPair (xpAttr "schemaLocation" xpText) $ xpTrees
+    ps = [ xpElem' "include"  $ xpWrap (Incl,  unIncl)  $ xpAttr' "schemaLocation" xpText
+         , xpElem' "import"   $ xpWrap (Imp,   unImp)   $ xpPair (xpAttr' "schemaLocation" xpText) (xpAttr' "namespace" xpText)
+         , xpElem' "redefine" $ xpWrap (Redef, unRedef) $ xpPair (xpAttr' "schemaLocation" xpText) xpTrees
          ]
 
 loadXmlSchema :: IO XmlSchema
@@ -244,10 +265,11 @@ loadXmlSchema
   = do
     s <- runX ( 
                 xunpickleDocument xpXmlSchema'
-                                  [ withValidate yes       -- validate source
-                                  , withTrace 1            -- trace processing steps
-                                  , withRemoveWS yes       -- remove redundant whitespace
-                                  , withPreserveComment no -- keep comments
+                                  [ withValidate yes        -- validate source
+                                  , withTrace 1             -- trace processing steps
+                                  , withRemoveWS yes        -- remove redundant whitespace
+                                  , withPreserveComment no  -- keep comments
+                                  , withCheckNamespaces yes -- check namespaces
                                   ] "example.xsd"
               )
     return $ toSchema $ head s
@@ -258,7 +280,7 @@ storeXmlSchema s
     _ <- runX ( constA (fromSchema s)
                 >>>
                 xpickleDocument   xpXmlSchema'
-                                  [ withIndent yes         -- indent generated xml
+                                  [ withIndent yes          -- indent generated xml
                                   ] "new-example.xsd"
               )
     return ()
