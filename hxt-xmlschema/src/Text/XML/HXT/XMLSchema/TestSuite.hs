c2d6d6c6d6b7039109b012ba9f949586c8317208
Thorben <thorbenguelck@gmx.net>
2012-04-04 15:41:54 +0200

    code cleanup and comments

---
+++ hxt-xmlschema/src/Text/XML/HXT/XMLSchema/TestSuite.hs
@@ -0,0 +1,31 @@
+{- |
+   Module     : Text.XML.HXT.XMLSchema.TestSuite
+   Copyright  : Copyright (C) 2005-2012 Uwe Schmidt
+   License    : MIT
+
+   Maintainer : Uwe Schmidt (uwe@fh-wedel.de)
+   Stability  : experimental
+   Portability: portable
+   Version    : $Id$
+
+-}
+
+module Text.XML.HXT.XMLSchema.TestSuite
+
+  ( module 
+  , 
+  , 
+  )
+
+where
+
+import ...    ( 
+              , 
+              , 
+              , 
+              )
+
+-- ----------------------------------------
+
+-- | ...
+
